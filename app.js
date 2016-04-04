299bc0b470fb0811b9b6f7c35824d0e89fbaf42b
Nick Ronnei <nronnei@gmail.com>
2016-04-04 00:29:34 -0400

    Puts the map in a website

---
+++ app.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var path = require('path');
+var tilestrata = require('tilestrata');
+var disk = require('tilestrata-disk');
+var mapnik = require('tilestrata-mapnik');
+var dependency = require('tilestrata-dependency');
+
+var strata = tilestrata();
+var app = express();
+
+// define layers
+//strata.layer('hillshade')
+//    .route('shade.png')
+//        .use(disk.cache({dir: './tiles/shade/'}))
+//        .use(mapnik({
+//            xml: './styles/test.xml',
+//            tileSize: 256,
+//            scale: 1
+//        }));
+//strata.layer('dem')
+//    .route('dem.png')
+//        .use(disk.cache({dir: './tiles/dem/'}))
+//        .use(mapnik({
+//            xml: './styles/dem.xml',
+//            tileSize: 256,
+//            scale: 1
+//        }));
+//strata.layer('sim1')
+//    .route('sim1.png')
+//        .use(disk.cache({dir: './tiles/sim1/'}))
+//        .use(mapnik({
+//            xml: './styles/sim1.xml',
+//            tileSize: 256,
+//            scale: 1
+//        }));
+//strata.layer('sim2')
+//    .route('sim2.png')
+//        .use(disk.cache({dir: './tiles/sim2/'}))
+//        .use(mapnik({
+//            xml: './styles/sim2.xml',
+//            tileSize: 256,
+//            scale: 1
+//        }));
+//strata.layer('sim3')
+//    .route('sim3.png')
+//        .use(disk.cache({dir: './tiles/sim3/'}))
+//        .use(mapnik({
+//            xml: './styles/sim3.xml',
+//            tileSize: 256,
+//            scale: 1
+//        }));
+//strata.layer('slope')
+//    .route('slope.png')
+//        .use(disk.cache({dir: './tiles/slope/'}))
+//        .use(mapnik({
+//            xml: './styles/slope.xml',
+//            tileSize: 256,
+//            scale: 1
+//        }));
+
+var staticPath = path.resolve(__dirname, './public/');
+app.use(express.static(staticPath));
+app.use(tilestrata.middleware({
+	server: strata,
+	prefix: ''
+}));
+
+app.listen(8080, function() {
+  console.log('Express server running on port 8080.');
+});
---
+++ public/app/_js/main.js
@@ -0,0 +1,138 @@
+////
+// Set up sidebar
+////
+
+$.fn.sidebar = function(options) {
+    var $sidebar = this;
+    var $tabs = $sidebar.find('ul.sidebar-tabs, .sidebar-tabs > ul');
+    var $container = $sidebar.children('.sidebar-content').first();
+
+    options = $.extend({
+        position: 'left'
+    }, options || {});
+
+    $sidebar.addClass('sidebar-' + options.position);
+
+    $tabs.children('li').children('a').on('click', function(e) {
+        e.preventDefault();
+        var $tab = $(this).closest('li');
+
+        if ($tab.hasClass('active'))
+            $sidebar.close();
+        else if (!$tab.hasClass('disabled'))
+            $sidebar.open(this.hash.slice(1), $tab);
+    });
+
+    $sidebar.find('.sidebar-close').on('click', function() {
+        $sidebar.close();
+    });
+
+    $sidebar.open = function(id, $tab) {
+        if (typeof $tab === 'undefined')
+            $tab = $tabs.find('li > a[href="#' + id + '"]').parent();
+
+        // hide old active contents
+        $container.children('.sidebar-pane.active').removeClass('active');
+
+        // show new content
+        $container.children('#' + id).addClass('active');
+
+        // remove old active highlights
+        $tabs.children('li.active').removeClass('active');
+
+        // set new highlight
+        $tab.addClass('active');
+
+        $sidebar.trigger('content', { 'id': id });
+
+        if ($sidebar.hasClass('collapsed')) {
+            // open sidebar
+            $sidebar.trigger('opening');
+            $sidebar.removeClass('collapsed');
+        }
+    };
+
+    $sidebar.close = function() {
+        // remove old active highlights
+        $tabs.children('li.active').removeClass('active');
+
+        if (!$sidebar.hasClass('collapsed')) {
+            // close sidebar
+            $sidebar.trigger('closing');
+            $sidebar.addClass('collapsed');
+        }
+    };
+
+    return $sidebar;
+};
+
+var sidebar = $('#sidebar').sidebar();
+
+
+////
+// Set static variables
+////
+
+// Setup layers
+// Mapbox layers
+var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>.'
+var mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibnJvbm5laSIsImEiOiJ2emt3WUY4In0.cRYAp7rDFZvRUBkExD5kqQ';
+var sat = L.tileLayer(mbUrl, {id: 'mapbox.satellite', attribution: mbAttr});
+var outdoors = L.tileLayer(mbUrl, {id: 'mapbox.outdoors', attribution: mbAttr});
+var emerald = L.tileLayer(mbUrl, {id: 'mapbox.emerald', attribution: mbAttr});
+// Basic SRTM layers
+var srtmAttr = ' | Hole-filled  seamless SRTM data V4.1, available from <a href="http://srtm.csi.cgiar.org/">CGIAR-CSI</a>.';
+var dem = L.tileLayer('http://ashtonjpm.geo.msu.edu/dem/{z}/{x}/{y}/dem.png', {attribution: srtmAttr});
+var hillshade = L.tileLayer('http://ashtonjpm.geo.msu.edu/hillshade/{z}/{x}/{y}/shade.png', {attribution: srtmAttr});
+var slope = L.tileLayer('http://ashtonjpm.geo.msu.edu/slope/{z}/{x}/{y}/slope.png', {attribution: srtmAttr});
+// SRTM error layers
+var errAttr = ' | Error Simulation by A. Shortridge & J. Messina';
+var sim1 = L.tileLayer('http://ashtonjpm.geo.msu.edu/sim1/{z}/{x}/{y}/sim1.png', {attribution: errAttr});
+var sim2 = L.tileLayer('http://ashtonjpm.geo.msu.edu/sim2/{z}/{x}/{y}/sim2.png', {attribution: errAttr});
+var sim3 = L.tileLayer('http://ashtonjpm.geo.msu.edu/sim3/{z}/{x}/{y}/sim3.png', {attribution: errAttr});
+
+////
+// Setup the map
+////
+
+// Set map bounds
+var sw = L.latLng(36,-84);
+var ne = L.latLng(43,-73);
+var bounds = L.latLngBounds(sw, ne);
+// Create map object
+var map = L.map('map', {
+	center: [39.8269, -77.2679],
+	zoom: 10,
+	minZoom: 7,
+	maxZoom: 14,
+	maxBounds: bounds,
+	layers: [emerald],
+	zoomControl: false,
+	attributionControl: false,
+	layerControl: false
+});
+// Graticle layer
+var gratStyle = {
+	fillColor: '#555555',
+	fillOpacity: 0.5,
+	color: '#555555',
+	opacity: 1
+};
+var gratLayer = L.geoJson(graticle, {style: gratStyle,
+																 onEachFeature: function (feature, layer) {
+																	 layer.bindPopup('<p><b>Tile: </b> '+feature.properties.tilename+'</p>'+
+																									 '<a target=_blank href="../_src/zips/'+feature.properties.tilename+'.zip">Download Error Sims</a>');}
+																});
+
+////
+// Set controls
+////
+
+// Zoom control
+L.control.zoom({position: 'bottomright'}).addTo(map);
+// Attribution control
+L.control.attribution({position: 'bottomleft', compact: true}).addTo(map);
+//Layer control
+var baseLayers = {'Mapbox Emerald': emerald, 'Mapbox Satellite': sat, 'Mapbox Outdoors': outdoors};
+var overlays = {'Download':gratLayer, 'Hillshade': hillshade, 'DEM': dem, 'Error Sim 1': sim1, 'Error Sim 2': sim2, 'Error Sim 3': sim3, 'Slope': slope};
+L.control.layers(baseLayers, overlays).addTo(map);
